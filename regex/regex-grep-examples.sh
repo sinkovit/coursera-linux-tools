ec81130654ab4fccb38498ebeea7397162522bf0
sinkovit <sinkovit@sdsc.edu>
2025-06-25 11:17:48 -0700

    Adds more regex examples

--- regex/regex-grep-examples.sh
+++ regex/regex-grep-examples.sh
@@ -1,6 +1,8 @@
 # Colorize grep output
 alias grep='grep --color=auto'
 
+# ----- regex characters -----
+
 # Matching multiple character with square brackets
 cat fruit.txt
 grep 'banana' fruit.txt
@@ -34,3 +36,49 @@ grep '.txt' dot.txt
 grep '\.txt' dot.txt
 grep '[.]txt' dot.txt
 grep -o '[.]txt' dot.txt
+
+# ----- regex quantifiers -----
+
+# Using full set of quantifiers
+grep -E 'ab*c' abc.txt
+grep -E 'ab+c' abc.txt
+grep -E 'ab?c' abc.txt
+grep -E 'ab{3}c' abc.txt
+grep -E 'ab{,3}c' abc.txt
+grep -E 'ab{3,}c' abc.txt
+grep -E 'ab{3,5}c' abc.txt
+
+# Doing the same thing as above with just * and |
+grep -E 'ab*c' abc.txt
+grep -E 'abb*c' abc.txt
+grep -E 'ac|abc' abc.txt
+grep -E 'abbbc' abc.txt
+grep -E 'abbbb*c' abc.txt
+grep -E 'ac|abc|abbc|abbbc' abc.txt
+grep -E 'abbbc|abbbbc|abbbbbc' abc.txt
+
+# Matching file names ending in .txt
+grep -E -o '[[:alpha:]]*[.]txt' dot.txt
+grep -E -o '[[:alpha:]]{6}[.]txt' dot.txt
+
+# ----- regex grouping, anchors, alternation & special characters -----
+
+# Grouping
+cat grp.txt
+grep -E 'A(xyz)?B' grp.txt
+grep -E 'A(xyz){2,3}B' grp.txt
+grep -E 'A(xyz)+B' grp.txt
+
+# Anchors
+cat fruit.txt
+grep '^pear' fruit.txt
+grep 'pear$' fruit.txt
+grep -E '^pear|pear$' fruit.txt
+
+# Alternation
+grep -E '^pear|pear$' fruit.txt
+
+# Escaping special characters
+grep -E '[[:alpha:]]+\*[[:alpha:]]+' spc.txt
+grep -E '[{(][0-9]+[})]' spc.txt
+grep -E '\{[0-9]+\}|\([0-9]+\)' spc.txt
