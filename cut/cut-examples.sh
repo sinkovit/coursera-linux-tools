a79e4fea70e78a88dc56bf9bee33ded000566f0d
sinkovit <sinkovit@sdsc.edu>
2025-07-15 20:25:47 -0700

    Updates cut whitespace example

--- cut/cut-examples.sh
+++ cut/cut-examples.sh
@@ -10,7 +10,10 @@ cat tab.tsv
 cut -f 1,3,5 tab.tsv
 cut -f 3-5 tab.tsv
 cut -f 1,3,5 whitespace.txt
-cut -f 1,3,5 -w whitespace.txt
+
+# Mac users may want to try the following
+# but be aware that it's not portable
+# cut -f 1,3,5 -w whitespace.txt
 
 # specifying a different delimiter
 cut -f 1,3,5 -d ',' comma.csv
