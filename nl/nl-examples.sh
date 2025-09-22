303ad001c86dc340b26906bea6b4c05cac77c4cf
Robert Sinkovits <sinkovit@sdsc.edu>
2025-09-22 16:39:37 -0700

    Update nl-examples.sh

--- nl/nl-examples.sh
+++ nl/nl-examples.sh
@@ -1,11 +1,21 @@
 # Basic nl functionality
 nl fruits.txt
 
-# Formatting nl output
+# left justified numbers
 nl -n ln fruits.txt
+
+# right justified numbers
 nl -n rz fruits.txt
+
+# ... plus adding a number width of 3
 nl -n rz -w 3 fruits.txt
+
+# ... plus starting numbering at 5
 nl -n rz -w 3 -v 5 fruits.txt
+
+# ... plus using an increment of 10
 nl -n rz -w 3 -v 5 -i 10 fruits.txt
+
+# Modifying the separator between number and line content
 nl -s ',' fruits.txt
 nl -s ' + ' fruits.txt
