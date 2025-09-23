6567111483dd8dfdaaebf654846c2679d1a2fa8e
Robert Sinkovits <sinkovit@sdsc.edu>
2025-09-22 17:00:37 -0700

    Update sort-examples.sh

--- sort/sort-examples.sh
+++ sort/sort-examples.sh
@@ -2,9 +2,9 @@
 sort fruit.txt
 
 # Effect of locale on sort results
-LC_ALL=C; sort fruit.txt; LC_ALL=
+LC_ALL=C sort fruit.txt
 
-# Sorting by column
+# Sorting by column (but note that we'll do this better below)
 sort -k1 snacks.txt
 sort -k2 snacks.txt
 sort -k3 snacks.txt
@@ -14,12 +14,12 @@ sort -k2n snacks.txt
 sort -k2nr snacks.txt
 sort -k2nr,2 snacks.txt
 
-# Sorting on multiple columnst
+# Sorting on multiple columns more reliably with -kx,x
 sort -k3,3 -k1,1 -k2,2 produce.txt
 sort -k2r,2 -k1,1 -k3,3 produce.txt
 
 # Defining custom delimiter
-sort -k3 -t ',' snacks.csv
+sort -k3,3 -t ',' snacks.csv
 
 # Removing duplicates after sorting
 sort -u fruit.txt
